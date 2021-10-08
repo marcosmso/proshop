8127bea2dd40a6149e1d708e875f101dcc0a7858
Marcos <marcosdp12@gmail.com>
2021-10-08 11:55:51 -0300

    implements pagination for products

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -6,12 +6,14 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET} from '../constants/productConstants'
+import Paginate from '../components/Paginate'
 
 const ProductListScreen = ({history, match}) => {
+  const pageNumber = match.params.pageNumber || 1
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
+  const { loading, error, products, pages, page } = productList
 
   const productDelete = useSelector(state => state.productDelete)
   const { loading: loadingDelete, error: errorDelete, success:successDelete } = productDelete
@@ -32,9 +34,17 @@ const ProductListScreen = ({history, match}) => {
     if(successCreate){
       history.push(`/admin/product/${createdProduct._id}/edit`)
     } else {
-      dispatch(listProducts())
+      dispatch(listProducts('', pageNumber))
     }
-  }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+  }, [
+    dispatch, 
+    history, 
+    userInfo, 
+    successDelete, 
+    successCreate, 
+    createdProduct, 
+    pageNumber
+  ])
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -67,6 +77,7 @@ const ProductListScreen = ({history, match}) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
+        <>
         <Table striped bordered hover responsive className="table-sm">
           <thead>
             <tr>
@@ -100,6 +111,8 @@ const ProductListScreen = ({history, match}) => {
             ))}
           </tbody>
         </Table>
+        <Paginate pages={pages} page={page} isAdmin={true}/>
+        </>
       )} 
     </>
   )
