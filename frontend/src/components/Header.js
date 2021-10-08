a9afcf6dec7fb1cfbb3354d675218d05a27aa549
Marcos <marcosdp12@gmail.com>
2021-10-08 11:02:19 -0300

    header fixes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import {LinkContainer} from 'react-router-bootstrap'
 import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap'
 import {logout} from '../actions/userActions'
+import SearchBox from './SearchBox'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -11,23 +13,21 @@ const Header = () => {
 
   const logoutHandler = () => {
     dispatch(logout())
-
   }
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+          <LinkContainer to='/'><Navbar.Brand>ProShop</Navbar.Brand></LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Route render={({history}) => <SearchBox history={history} />}/>          
+            </Nav>
+            <Nav>
               <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"/>Cart
-                </Nav.Link>
+                <Nav.Link><i className="fas fa-shopping-cart"/> Cart</Nav.Link>
               </LinkContainer> 
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
@@ -38,26 +38,24 @@ const Header = () => {
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
-              ) :
+              ) : (
                 <LinkContainer to='/login'>
                   <Nav.Link ><i className="fas fa-user"/>Sign In</Nav.Link>
                 </LinkContainer>
-              }
-              {
-                userInfo && userInfo.isAdmin && (
-                  <NavDropdown title='Admin' id='adminmenu'>
-                    <LinkContainer to='/admin/userlist'>
-                      <NavDropdown.Item>Users</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to='/admin/productlist'>
-                      <NavDropdown.Item>Products</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to='/admin/orderlist'>
-                      <NavDropdown.Item>Orders</NavDropdown.Item>
-                    </LinkContainer>
-                  </NavDropdown>
-                )
-              }
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
